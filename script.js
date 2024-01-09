6595ea46e6ecf63ddaa72ad098d4fa60c79a5a43
SihamBelkhadra <153821542+SihamBelkhadra@users.noreply.github.com>
2024-01-09 22:22:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
             name: 'Pokèmon',
             location: {
                 // decomment the following and add coordinates:
-                lat: 35.561722,
-                lng: -5.362444,
+                lat: 35.614391,
+                lng: -5.2789028,
             },
         },
     ];
